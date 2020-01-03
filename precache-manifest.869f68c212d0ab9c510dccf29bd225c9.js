c67e8026af0ec3f1e154dd92529302a5191f732b
Tomas Sakinis <tomas.sakinis@geniussports.com>
2020-01-03 22:22:36 +0200

    Updates

---
+++ precache-manifest.869f68c212d0ab9c510dccf29bd225c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4eb5aa2b45107a0a5502f1449e76262",
+    "url": "/milzinas/index.html"
+  },
+  {
+    "revision": "4771c14fbe78e3302b01",
+    "url": "/milzinas/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "705dc6c97866109e2be1",
+    "url": "/milzinas/static/js/2.4db4d2da.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/milzinas/static/js/2.4db4d2da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4771c14fbe78e3302b01",
+    "url": "/milzinas/static/js/main.e9818323.chunk.js"
+  },
+  {
+    "revision": "0aee512e9c334397a0c4",
+    "url": "/milzinas/static/js/runtime-main.52f30527.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/milzinas/static/media/logo.5d5d9eef.svg"
+  }
+]);
